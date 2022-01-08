6847aff67f121a5513e251bb4fd275441bd1635b
Manuel Guariglia <manuelguariglia@gmail.com>
2022-01-08 10:38:36 +0100

    header started

--- index.php
+++ index.php
@@ -5,13 +5,21 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dischi Ajax</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <!-- General Container -->
   <div id="app">
-      <h1>
-        {{word}}
-      </h1>
+    <header>
+      <img src="img/logo.png" alt="Spotify Web">
+      <select v-model="textToSearch" @change="$emit('sendSearch',textToSearch)" class="form-select form-select-lg mb-3 w-25" aria-label=".form-select-lg example">
+        <option value="all" selected>Tutti i generi</option>
+        <option value="pop">Pop</option>
+        <option value="rock">Rock</option>
+        <option value="metal">Metal</option>
+        <option value="jazz">Jazz</option>
+      </select>
+    </header>
   </div>
   <!-- /General Container -->
   
